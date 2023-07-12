2489d8efa850af3e70b818485ff4a05c25886e9c
Ricardo Rivas <richistron@gmail.com>
2023-07-11 21:40:42 -0700

    Adds bubble sort

---
+++ SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,20 @@
+﻿namespace SortingAlgorithms;
+
+public static class BubbleSort
+{
+    public static int[] Sort(int[] items)
+    {
+        for (int i = 0; i < items.Length - 1; i++)
+        {
+            for (int j = i + 1; j < items.Length; j++)
+            {
+                if (items[i] > items[j])
+                {
+                    (items[i], items[j]) = (items[j], items[i]);
+                }
+            }
+        }
+
+        return items;
+    }
+}
---
+++ Specs/BubbleSortTest.cs
@@ -0,0 +1,12 @@
+﻿using SortingAlgorithms;
+
+namespace Specs;
+
+[TestFixture]
+public class BubbleSortTest
+{
+    [Test]
+    [TestCase(new[] { 9, 5, 3, 0 }, new[] { 0, 3, 5, 9 })]
+    [TestCase(new[] { 100, 500, 1 }, new[] { 1, 100, 500 })]
+    public void Sort_Array(int[] input, int[] output) => Assert.That(BubbleSort.Sort(input), Is.EquivalentTo(output));
+}
